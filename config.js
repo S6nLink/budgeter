efec6bba8fe1bc39b166ac5ef75ad4264df09f1f
Shogo.Saito <66563780+S6nLink@users.noreply.github.com>
2026-05-04 00:22:33 +0900

    画面と設定修正

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBp5SiirSsyKwpaYVl_03bqCkdAtZAwczY",
+    authDomain: "budgeter-43efe.firebaseapp.com",
+    projectId: "budgeter-43efe",
+    storageBucket: "budgeter-43efe.firebasestorage.app",
+    messagingSenderId: "170335209237",
+    appId: "1:170335209237:web:6e0a6db052cc739615cb1c",
+    measurementId: "G-8PDWWHDFY4"
+    };
